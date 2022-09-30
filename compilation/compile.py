632b501a968eccad413193766c0e796d863c21cd
Aditya Painuly <adivp7@gmail.com>
2022-09-30 21:19:21 +0530

    modified:   compilation/compile.py

--- compilation/compile.py
+++ compilation/compile.py
@@ -1,9 +1,13 @@
 import subprocess
+import os
 import json
 def main():
+    # print(os.getcwd())
     json_text=open("test.json").read()
-    json.loads(json_text)
-    print(json_text)
+    json_data=json.loads(json_text)
+    # print(json_data)
+    code_to_compile=json_data['code']
+    print(code_to_compile)
 
 if __name__ == "__main__":
     main()
