24a7bbceaaa19f22aeed3730cae7077bcd83decc
Aditya Painuly <adivp7@gmail.com>
2022-09-30 21:53:02 +0530

    deleted:    compilation/a.exe 	new file:   compilation/code.cpp 	modified:   compilation/compile.py 	deleted:    compilation/test.cpp 	modified:   compilation/test.json

--- compilation/compile.py
+++ compilation/compile.py
@@ -1,13 +1,29 @@
 import subprocess
 import os
 import json
+
+
 def main():
     # print(os.getcwd())
-    json_text=open("test.json").read()
-    json_data=json.loads(json_text)
+    json_text = open("test.json").read()
+    json_data = json.loads(json_text)
     # print(json_data)
-    code_to_compile=json_data['code']
-    print(code_to_compile)
+    code_lang = json_data['lang']
+    code_text = json_data['code']
+    input_text = json_data['input']
+    # print(code_text)
+    lang_func = {"cpp": compile_cpp}
+    lang_func[code_lang](code_text, input_text)
+
+
+def compile_cpp(code, input):
+    code_file = open("code.cpp", "w")
+    code_file.write(code)
+    compilation = subprocess.run(
+        ["g++", "code.cpp", "-o", "code.out", "-Wall", "-O2"])
+    if compilation.returncode != 0:  # compilation failed
+        print(compilation.)
+
 
 if __name__ == "__main__":
     main()
