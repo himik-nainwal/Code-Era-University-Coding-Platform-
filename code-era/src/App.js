49bcc1c68731b3f256893ef1fb660ffe196b1370
10K-K10 <ketanrai420@gmail.com>
2022-09-30 13:34:06 +0530

    navbar darkmode

--- code-era/src/App.js
+++ code-era/src/App.js
@@ -2,9 +2,8 @@ import './App.css';
 
 function App() {
   return <>
-  <h1>Code Era</h1>
   {/* navbar */}
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
