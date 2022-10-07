cc4e93d294d62f975e6cad5c9f9205a9dd5c77ad
Harshit <harshitbhatt972@gmail.com>
2022-10-08 01:39:55 +0530

    working on editor in problem section

--- code-era/src/app/Problem.js
+++ code-era/src/app/Problem.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Editor, { DiffEditor, useMonaco, loader } from "@monaco-editor/react";
 
 function Problem() {
+  const [code, setCode] = useState("");
+
+  useEffect(() => {
+    window.addEventListener("keydown", (e) => {
+      if (e.ctrlKey || e.metaKey) {
+        if (String.fromCharCode(e.which).toLowerCase() === "s") {
+          e.preventDefault();
+        }
+      }
+    });
+  }, []);
+
   return (
     <>
       <div class="modal-body row">
@@ -8,7 +21,18 @@ function Problem() {
           {/* <!-- Your first column here --> */}
           <h1>Question part</h1>
         </div>
-        <div class="col-md-6">{/* <!-- Your second column here --> */}</div>
+        <div class="col-md-6">
+          <Editor
+            value={code}
+            onChange={(e) => {
+              setCode(e);
+            }}
+            height="90vh"
+            defaultLanguage="javascript"
+            language="cpp"
+            defaultValue="// some comment"
+          />
+        </div>
       </div>
     </>
   );
