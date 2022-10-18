20e3b334eb51f4b2ccd8c6948730bd862a8dff60
Harshit <harshitbhatt972@gmail.com>
2022-10-18 22:22:46 +0530

    problemset edited

--- code-era/src/components/navbar/RootNavbar.js
+++ code-era/src/components/navbar/RootNavbar.js
@@ -4,7 +4,13 @@ import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 function RootNavbar() {
   return (
     <>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar
+        sticky="top"
+        collapseOnSelect
+        expand="lg"
+        bg="dark"
+        variant="dark"
+      >
         <Container>
           <Navbar.Brand href="/">Code Era</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
