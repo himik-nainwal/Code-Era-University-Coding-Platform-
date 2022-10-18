dea18a69dc9e99e33b5ecba0ec58d824fa62448e
himik-nainwal <himithnainwal@gmail.com>
2022-10-18 14:42:04 +0530

    up

--- Scraping/scrap.py
+++ Scraping/scrap.py
@@ -4,7 +4,7 @@ import smtplib
 import time
 
 
-import bs4
+from bs4 import BeautifulSoup
 import colorama
 import requests
 
@@ -14,7 +14,7 @@ from ebooklib import epub
 # from utils import *
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from utils import *
+# from utils import *
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
@@ -25,3 +25,12 @@ from selenium.webdriver.chrome.service import Service
 questions_url='https://leetcode.com/problemset/all/'
 api='https://leetcode.com/api/problems/algorithms/'
 
+# Setting up Selenium Webdriver
+CHROMEDRIVER_PATH=r"C://Chrome_driver//chromedriver.exe"
+options=Options()
+options.headless=True
+options.add_argument("--log-level=3")
+driver=webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,options=options)
+
+# Having a configuration file to track
+completed_till=read_tracker("track.conf")
--- code-era/src/pages/Home.js
+++ code-era/src/pages/Home.js
@@ -1,6 +1,10 @@
 import React, { useEffect } from "react";
-import axios from "../utils/axios";
+import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBTypography, MDBIcon } from 'mdb-react-ui-kit';
 
+import axios from "../utils/axios";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card'
+import profile_pic from '../assets/IMG_9711.jpg'
 function Home() {
   useEffect(() => {
     const fetchData = async () => {
@@ -10,7 +14,63 @@ function Home() {
     fetchData();
   }, []);
 
-  return <div>Home</div>;
+  return( 
+    <section className="vh-100" style={{ backgroundColor: '#f4f5f7' }}>
+    <MDBContainer className="py-5 h-100">
+      <MDBRow className="justify-content-center align-items-center h-100">
+        <MDBCol lg="6" className="mb-4 mb-lg-0">
+          <MDBCard className="mb-3" style={{ borderRadius: '.5rem' }}>
+            <MDBRow className="g-0">
+              <MDBCol md="4" className="gradient-custom text-center text-white"
+                style={{ borderTopLeftRadius: '.5rem', borderBottomLeftRadius: '.5rem' }}>
+                <MDBCardImage src="C:\Users\Simik\OneDrive\Documents\Code-Era-University-Coding-Platform-\code-era\src\assets\IMG_9711.jpg"
+                  alt="Avatar" className="my-5" style={{ width: '80px' }} fluid />
+                <MDBTypography tag="h5">Marie Horwitz</MDBTypography>
+                <MDBCardText>Web Designer</MDBCardText>
+                <MDBIcon far icon="edit mb-5" />
+              </MDBCol>
+              <MDBCol md="8">
+                <MDBCardBody className="p-4">
+                  <MDBTypography tag="h6">Information</MDBTypography>
+                  <hr className="mt-0 mb-4" />
+                  <MDBRow className="pt-1">
+                    <MDBCol size="6" className="mb-3">
+                      <MDBTypography tag="h6">Email</MDBTypography>
+                      <MDBCardText className="text-muted">info@example.com</MDBCardText>
+                    </MDBCol>
+                    <MDBCol size="6" className="mb-3">
+                      <MDBTypography tag="h6">Phone</MDBTypography>
+                      <MDBCardText className="text-muted">123 456 789</MDBCardText>
+                    </MDBCol>
+                  </MDBRow>
+
+                  <MDBTypography tag="h6">Information</MDBTypography>
+                  <hr className="mt-0 mb-4" />
+                  <MDBRow className="pt-1">
+                    <MDBCol size="6" className="mb-3">
+                      <MDBTypography tag="h6">Email</MDBTypography>
+                      <MDBCardText className="text-muted">info@example.com</MDBCardText>
+                    </MDBCol>
+                    <MDBCol size="6" className="mb-3">
+                      <MDBTypography tag="h6">Phone</MDBTypography>
+                      <MDBCardText className="text-muted">123 456 789</MDBCardText>
+                    </MDBCol>
+                  </MDBRow>
+
+                  <div className="d-flex justify-content-start">
+                    <a href="#!"><MDBIcon fab icon="facebook me-3" size="lg" /></a>
+                    <a href="#!"><MDBIcon fab icon="twitter me-3" size="lg" /></a>
+                    <a href="#!"><MDBIcon fab icon="instagram me-3" size="lg" /></a>
+                  </div>
+                </MDBCardBody>
+              </MDBCol>
+            </MDBRow>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  </section>
+);
 }
 
 export default Home;
