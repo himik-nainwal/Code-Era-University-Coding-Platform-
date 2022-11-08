26a35100b2c2e3fc574eb010e10576c731be05c9
himik-nainwal <himithnainwal@gmail.com>
2022-11-08 14:31:16 +0530

    added

--- code-era/src/pages/Problemset.js
+++ code-era/src/pages/Problemset.js
@@ -1,10 +1,26 @@
-import React from "react";
-function Problemset() {
+import React from 'react'
+import "../styles/prob.css"
+const Problemset = () => {
   return (
-    <>
-      <div>problemset</div>
-    </>
-  );
+    // <app-navbar></app-navbar>
+    <div class="box">
+      <table class ="table table-striped">
+        <thead>
+          <tr>
+            <th class="header">ID</th>
+            <th class="header">Title</th>
+            <th class="header">Difficulty</th>
+            <th class="header">Actions</th>
+
+          </tr>
+        </thead>
+        <tbody>
+          
+        </tbody>
+
+      </table>
+    </div>
+  )
 }
 
-export default Problemset;
+export default Problemset
