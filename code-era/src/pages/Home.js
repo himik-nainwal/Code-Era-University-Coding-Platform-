40c60aa91c7f40a462083943c6558706b42887b0
himik-nainwal <himithnainwal@gmail.com>
2022-11-10 14:26:37 +0530

    added

--- code-era/src/pages/Home.js
+++ code-era/src/pages/Home.js
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import "../styles/home.css"
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='main-container'><center><h1>Welcome to Code-Era</h1></center>
+    <h4>
+      Currently working on page 
+    </h4>
+    </div>
+    
   )
 }
 
