b3d5a29733173db8bc03be301ba54b7357aee93c
himik-nainwal <himithnainwal@gmail.com>
2022-11-28 13:43:43 +0530

    schema added

---
+++ login-backend/codeDetails.js
@@ -0,0 +1,43 @@
+const mongoose =require("mongoose");
+const codekaschema = new mongoose.Schema(
+    {
+        ques_id:{
+            type: Number,
+            required: true,
+            unique:true,
+        },
+        
+        cpp_boilerplate:{
+            type: String,
+        },
+        
+        correct_code:{
+            type: String,
+            // required: true,
+        },
+
+        custom_judge:{
+            type: String,
+        },
+        example_test_case_input:{
+            type:String,
+
+        },
+        example_test_case_output:{
+            type:String,
+        },
+        all_test_cases_input:{
+            type:String,
+        },
+        all_test_cases_output:{
+            type:String,
+        },
+    },
+    {
+        timestamps:true,
+    },
+    {
+        collection: "codes",
+    }
+);
+module.exports = mongoose.model("codes",codekaschema);
