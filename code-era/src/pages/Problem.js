a2553010404fa154bd997be8c69885c929934757
Aditya Painuly <adivp7@gmail.com>
2022-11-28 20:01:37 +0530

    sahi code

--- code-era/src/pages/Problem.js
+++ code-era/src/pages/Problem.js
@@ -67,51 +67,41 @@ function Problem() {
       stdin: btoa(""),
     };
 
-    // console.log(reqData);
-
-    // const options = {
-    //   method: "POST",
-    //   url: "https://judge0-ce.p.rapidapi.com/submissions",
-    //   params: { base64_encoded: "true", fields: "*" },
-    //   headers: {
-    //     "content-type": "application/json",
-    //     "Content-Type": "application/json",
-    //     "X-RapidAPI-Key": "e7aba527c2msh4791c3306942553p17f71bjsnd62f0d24477a",
-    //     "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
-    //   },
-    //   data: reqData,
-    // };
-
-    // axios
-    //   .request(options)
-    //   .then(function (response) {
-    //     console.log(response.data);
-    //   })
-    //   .catch(function (error) {
-    //     console.error(error);
-    //   });
+    console.log(reqData);
 
     const options = {
-      method: "POST",
-      url: "https://judge0-ce.p.rapidapi.com/submissions",
-      params: { base64_encoded: "true", fields: "*" },
+      method: 'POST',
+      url: 'https://judge0-ce.p.rapidapi.com/submissions',
+      params: { base64_encoded: 'true', fields: '*' },
       headers: {
-        "content-type": "application/json",
-        "Content-Type": "application/json",
-        "X-RapidAPI-Key": "e7aba527c2msh4791c3306942553p17f71bjsnd62f0d24477a",
-        "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
+        'content-type': 'application/json',
+        'Content-Type': 'application/json',
+        'X-RapidAPI-Key': 'e7aba527c2msh4791c3306942553p17f71bjsnd62f0d24477a',
+        'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
       },
-      data: '{"language_id":52,"source_code":"I2luY2x1ZGUgPHN0ZGlvLmg+CgppbnQgbWFpbih2b2lkKSB7CiAgY2hhciBuYW1lWzEwXTsKICBzY2FuZigiJXMiLCBuYW1lKTsKICBwcmludGYoImhlbGxvLCAlc1xuIiwgbmFtZSk7CiAgcmV0dXJuIDA7Cn0=","stdin":"SnVkZ2Uw"}',
+      data: reqData,
     };
 
-    axios
-      .request(options)
-      .then(function (response) {
+    // axios.request(options).then(function (response) {
+    //   console.log(response.data);
+    // }).catch(function (error) {
+    //   console.error(error);
+    // });
+
+    async function createSubmission() {
+      try {
+        let response = await axios.request(options);
         console.log(response.data);
-      })
-      .catch(function (error) {
+      }
+      catch (error) {
         console.error(error);
-      });
+      }
+    }
+
+    createSubmission();
+
+    //get token from createSubmission
+
   };
 
   useEffect(() => {
