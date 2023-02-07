085e69634aafdc3dcbcde82a9073828c6007eec7
himik-nainwal <himithnainwal@gmail.com>
2023-02-07 14:09:46 +0530

    added

--- code-era/src/pages/Problemset.js
+++ code-era/src/pages/Problemset.js
@@ -6,14 +6,45 @@ import { BsBoxArrowInRight } from "react-icons/bs";
 
 const Problemset = () => {
   const [problems, setProblems] = useState([]);
-  const questionIds = [];
+  const [questionIds , setQuestionIds] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       fetch(`http://localhost:5000/problems`)
         .then((res) => res.json())
         .then((res) => setProblems(res.data));
+        
     };
     fetchData();
+  }, []);
+  useEffect(() => {
+    let mounted = true;
+    if (mounted) {
+      const fn = () => {
+        const url = "http://localhost:5000/userData";
+        const data = fetch(url, {
+          method: "POST",
+          crossDomain: true,
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            "Access-Control-Allow-Origin": "*",
+          },
+          body: JSON.stringify({
+            token: window.localStorage.getItem("token"),
+          }),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            setQuestionIds(data.data.questionIds);
+            
+          });
+      };
+      fn();
+    }
+
+    return () => {
+      mounted = false;
+    };
   }, []);
 
   //sorting questions
