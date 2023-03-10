82992fbd4d0bcbc604ac46c06741b96232db1706
10K-K10 <ketanrai420@gmail.com>
2023-03-10 21:02:25 +0530

    newww

--- code-era/src/pages/Problemset.js
+++ code-era/src/pages/Problemset.js
@@ -8,9 +8,10 @@ import DropdownMenu from "../components/dropdown/DropdownMenu";
 const Problemset = () => {
   const [problems, setProblems] = useState([]);
   const [questionIds, setQuestionIds] = useState([]);
+  const [startPgNo, setStartPgNo] = useState(1);
+  const [endPgNo, setEndPgNo] = useState(15);
 
   const [selectedOption, setSelectedOption] = useState("");
-  const [sortedQuestions, setSortedQuestions] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -61,36 +62,36 @@ const Problemset = () => {
   //   return 0;
   // }
 
-  const handleOptionSelected = (option) => {
-    setSelectedOption(option);
-    const sorted = [...problems].sort((a, b) => {
-      const aLevel = a.difficulty_level;
-      const bLevel = b.difficulty_level;
-      if (option === "Easy") {
-        return aLevel - bLevel;
-      } else if (option === "Medium") {
-        return Math.abs(2 - aLevel) - Math.abs(2 - bLevel);
-      } else if (option === "Hard") {
-        return bLevel - aLevel;
-      } else {
-        return 0;
-      }
-    });
-    setSortedQuestions(sorted);
-  };
+  // const handleOptionSelected = (option) => {
+  //   setSelectedOption(option);
+  //   const sorted = [...problems].sort((a, b) => {
+  //     const aLevel = a.difficulty_level;
+  //     const bLevel = b.difficulty_level;
+  //     if (option === "Easy") {
+  //       return aLevel - bLevel;
+  //     } else if (option === "Medium") {
+  //       return Math.abs(2 - aLevel) - Math.abs(2 - bLevel);
+  //     } else if (option === "Hard") {
+  //       return bLevel - aLevel;
+  //     } else {
+  //       return 0;
+  //     }
+  //   });
+  //   setSortedQuestions(sorted);
+  // };
 
-  const getDifficultyLevel = (level) => {
-    switch (level) {
-      case 1:
-        return 1;
-      case 2:
-        return 2;
-      case 3:
-        return 3;
-      default:
-        return 0;
-    }
-  };
+  // const getDifficultyLevel = (level) => {
+  //   switch (level) {
+  //     case 1:
+  //       return 1;
+  //     case 2:
+  //       return 2;
+  //     case 3:
+  //       return 3;
+  //     default:
+  //       return 0;
+  //   }
+  // };
 
   return (
     <>
@@ -98,7 +99,7 @@ const Problemset = () => {
         className="p-4 d-flex justify-content-center px-4"
         style={{ background: "#141e30" }}
       >
-        <DropdownMenu onOptionSelected={handleOptionSelected} />
+        {/* <DropdownMenu onOptionSelected={handleOptionSelected} /> */}
       </div>
       <div>
         {problems.length ? (
@@ -116,91 +117,73 @@ const Problemset = () => {
                   </tr>
                 </thead>
                 <tbody>
-                  {sortedQuestions.length > 1
-                    ? sortedQuestions.map((problem, i) => (
-                        <tr key={i}>
-                          <td>{i + 1}</td>
-                          <td>{problem?.question_title}</td>
-                          <td
-                            className={
-                              problem?.difficulty_level === 1
-                                ? "Easy"
-                                : problem?.difficulty_level === 2
-                                ? "Medium"
-                                : "Hard"
-                            }
-                          >
-                            {problem?.difficulty_level === 1
-                              ? "Easy"
-                              : problem?.difficulty_level === 2
-                              ? "Medium"
-                              : "Hard"}
-                          </td>
-                          <td
-                            className={
-                              questionIds.includes(problem?.ques_id)
-                                ? "Solved"
-                                : "Unsolved"
-                            }
-                          >
-                            {questionIds.includes(problem?.ques_id)
-                              ? "Solved"
-                              : "Unsolved"}
-                          </td>
-                          <td>
-                            <Link
-                              style={{ textDecoration: "none", color: "black" }}
-                              to={`/problem/${problem?.ques_id}`}
-                            >
-                              View Problem
-                              <BsBoxArrowInRight className="ms-2" />
-                            </Link>
-                          </td>
-                        </tr>
-                      ))
-                    : problems.map((problem, i) => (
-                        <tr key={i}>
-                          <td>{i + 1}</td>
-                          <td>{problem?.question_title}</td>
-                          <td
-                            className={
-                              problem?.difficulty_level === 1
-                                ? "Easy"
-                                : problem?.difficulty_level === 2
-                                ? "Medium"
-                                : "Hard"
-                            }
-                          >
-                            {problem?.difficulty_level === 1
-                              ? "Easy"
-                              : problem?.difficulty_level === 2
-                              ? "Medium"
-                              : "Hard"}
-                          </td>
-                          <td
-                            className={
-                              questionIds.includes(problem?.ques_id)
-                                ? "Solved"
-                                : "Unsolved"
-                            }
-                          >
-                            {questionIds.includes(problem?.ques_id)
-                              ? "Solved"
-                              : "Unsolved"}
-                          </td>
-                          <td>
-                            <Link
-                              style={{ textDecoration: "none", color: "black" }}
-                              to={`/problem/${problem?.ques_id}`}
-                            >
-                              View Problem
-                              <BsBoxArrowInRight className="ms-2" />
-                            </Link>
-                          </td>
-                        </tr>
-                      ))}
+                  {problems.slice(startPgNo, endPgNo + 1).map((problem, i) => (
+                    <tr key={i}>
+                      <td>{problem?.ques_id}</td>
+                      <td>{problem?.question_title}</td>
+                      <td
+                        className={
+                          problem?.difficulty_level === 1
+                            ? "Easy"
+                            : problem?.difficulty_level === 2
+                            ? "Medium"
+                            : "Hard"
+                        }
+                      >
+                        {problem?.difficulty_level === 1
+                          ? "Easy"
+                          : problem?.difficulty_level === 2
+                          ? "Medium"
+                          : "Hard"}
+                      </td>
+                      <td
+                        className={
+                          questionIds.includes(problem?.ques_id)
+                            ? "Solved"
+                            : "Unsolved"
+                        }
+                      >
+                        {questionIds.includes(problem?.ques_id)
+                          ? "Solved"
+                          : "Unsolved"}
+                      </td>
+                      <td>
+                        <Link
+                          style={{ textDecoration: "none", color: "black" }}
+                          to={`/problem/${problem?.ques_id}`}
+                        >
+                          View Problem
+                          <BsBoxArrowInRight className="ms-2" />
+                        </Link>
+                      </td>
+                    </tr>
+                  ))}
                 </tbody>
               </table>
+              <div className="row">
+                <div className="col-2">
+                  <button
+                    className="btn btn-info btn-sm"
+                    disabled={startPgNo == 1}
+                    onClick={() => {
+                      setStartPgNo(startPgNo - 15);
+                      setEndPgNo(endPgNo - 15);
+                    }}
+                  >
+                    Prev
+                  </button>
+                  <button
+                    className="btn btn-info btn-sm"
+                    disabled={startPgNo + 15 > problems.length}
+                    onClick={() => {
+                      setStartPgNo(startPgNo + 15);
+                      setEndPgNo(endPgNo + 15);
+                    }}
+                  >
+                    Next
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         ) : (
