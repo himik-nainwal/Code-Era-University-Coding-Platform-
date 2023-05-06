f5c3cd97379e713e7a9ae799228febb810cfdca0
Harshit <harshitbhatt972@gmail.com>
2023-05-06 12:25:28 +0530

    removed hawk and removed codeera-backend

--- code-era-backend/api/routers/auth.router.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const { schemas, validateBody} = require ("../../utils/SchemaValidator");
-const authRouter = require("../controllers/auth");
-
-router.post("/",validateBody(schemas.userLoginSchema),authRouter.login).
-post("/register",validateBody(schemas.userRegisterSchema),authRouter.register);
-
-
-module.exports = router;
--- code-era-backend/api/routers/index.js
+++
@@ -1,7 +0,0 @@
-const rootRouter = require("express").Router();
-
-const authRouter = require("./auth.router");
-
-rootRouter.use("/auth", authRouter);
-
-module.exports = rootRouter;
